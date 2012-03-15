4b902b61f5e902f50a22dedc09c3505a078909f4
Mirek Kratochvil <exa.exa@gmail.com>
2012-03-15 12:32:54 +0100

    specification update

--- include/codecrypt.h
+++ include/codecrypt.h
@@ -18,14 +18,13 @@ struct ccr_nd_pubkey {
 struct ccr_nd_privkey {
 };
 
-int ccr_gen_mce(struct ccr_mce_pubkey*, struct ccr_mce_privkey*);
-int ccr_gen_nd(struct ccr_nd_pubkey*, struct ccr_nd_privkey*);
+int ccr_mce_gen(struct ccr_mce_pubkey*, struct ccr_mce_privkey*);
+int ccr_mce_encrypt(struct ccr_mce_pubkey*, const char*, char*);
+int ccr_mce_decrypt(struct ccr_mce_privkey*, const char*, char*);
 
-int ccr_encrypt(struct ccr_mce_pubkey*, const char*, char*);
-int ccr_decrypt(struct ccr_mce_privkey*, const char*, char*);
-
-int ccr_sign(struct ccr_nd_privkey*, const char*, char*);
-int ccr_read_signature(struct ccr_nd_pubkey*, const char*, char*);
+int ccr_nd_gen(struct ccr_nd_pubkey*, struct ccr_nd_privkey*);
+int ccr_nd_encrypt(struct ccr_nd_privkey*, const char*, char*);
+int ccr_nd_decrypt(struct ccr_nd_pubkey*, const char*, char*);
 
 #ifdef __cplusplus
 }
