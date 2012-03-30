85baaa3ac32201767c5eb320204fa37771551430
Mirek Kratochvil <exa.exa@gmail.com>
2012-03-30 15:01:01 +0200

    more of C++ outbreak

--- include/codecrypt.h
+++ include/codecrypt.h
@@ -2,87 +2,65 @@
 #ifndef _CODECRYPT_H_
 #define _CODECRYPT_H_
 
-#include <stddef.h>
-#include <stdint.h>
+#include <vector>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+namespace ccr {
 
-	/* codecrypt matrix/vector/whatever type */
-	typedef uint8_t* ccr_mtx;
-	/* permutation as a list of transpositions */
-	typedef int* ccr_perm;
+	typedef std::vector<bool> bvector;
+	//for broken/old/weird STL uncomment this:
+	//typedef std::bit_vector bvector;
+	//TODO ifdef
+	
+	class matrix : public std::vector<bvector> {
 
-	/* macros for faster allocation/accessing */
-#define ccr_mtx_alloc_size(veclen,nvec) ((1+(((veclen)-1)/8))*(nvec))
-#define ccr_mtx_vec_offset ccr_mtx_alloc_size
-
-	struct ccr_mce_pubkey {
-		/* params */
-		int n, k, t;
-
-		/* n*k G' pubkey matrix */
-		ccr_mtx g;
 	};
 
-	struct ccr_mce_privkey {
-		/* params, n and t are input params */
-		int n, k, t;
+	class permutation : public vector<unsigned int> {
 
-		/* goppa polynomial of degree t */
-		ccr_mtx poly;
-
-		/* inverse of S matrix */
-		ccr_mtx sinv;
-
-		/* inverse of P permutation */
-		ccr_perm pinv;
-
-		/* systematic form permutation (inv.) */
-		ccr_perm psys;
-
-		/* parity check matrix */
-		ccr_mtx h;
 	};
 
-	struct ccr_nd_pubkey {
-		/* params */
-		int n, k, t;
+	class polynomial : public bvector {
 
-		/* pubkey matrix */
-		ccr_mtx h;
 	};
 
-	struct ccr_nd_privkey {
-		/* params */
-		int n, k, t;
+	namespace mce {
+		class privkey {
+		public:
+			matrix 
 
-		/* goppa polynomial of degree t */
-		ccr_mtx poly;
+			int decrypt(const bvector&, bvector&);
+		};
 
-		/* inverse of S matrix */
-		ccr_mtx sinv;
+		class pubkey {
+		public:
+			matrix G;
+			int t;
+			int encrypt(const bvector&, bvector&);
+		};
 
-		/* inverse of P permutation */
-		ccr_perm pinv;
-	};
+		int generate(pubkey&,privkey&);
+	}
 
-	/* actual functions */
-	int ccr_mce_gen (struct ccr_mce_pubkey*, struct ccr_mce_privkey*);
-	int ccr_mce_encrypt (struct ccr_mce_pubkey*, const uint8_t*, uint8_t*);
-	int ccr_mce_decrypt (struct ccr_mce_privkey*, const uint8_t*, uint8_t*);
+	namespace nd {
+		class privkey {
 
-	int ccr_nd_gen (struct ccr_nd_pubkey*, struct ccr_nd_privkey*);
-	int ccr_nd_encrypt (struct ccr_nd_privkey*, const uint8_t*, uint8_t*);
-	int ccr_nd_decrypt (struct ccr_nd_pubkey*, const uint8_t*, uint8_t*);
+			int decrypt(const bvector&, bvector&);
+		};
 
-	void ccr_set_log_func (void (*) (const char*) );
-	void ccr_set_internal_allocator (void* (*) (size_t), void (*) (void*) );
+		class pubkey {
+		public:
+			matrix H;
+			int t;
 
-#ifdef __cplusplus
-}
-#endif
+			int encrypt(const bvector&, bvector&);
+		};
 
-#endif /* _CODECRYPT_H_ */
+		int generate(pubkey&,privkey&);
+	}
+
+	//TODO entropy sources
+
+} //namespace CCR
+
+#endif // _CODECRYPT_H_
 
--- lib/log.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef _CCR_LOG_H_
-#define _CCR_LOG_H_
-
-void ccr_log (const char*, ...);
-
-#endif
-
