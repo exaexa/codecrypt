7f0121294c6df1e1959e7585b9471b46634ca48a
Mirek Kratochvil <exa.exa@gmail.com>
2012-04-01 20:55:18 +0200

    kill warnings

--- lib/mce.cpp
+++ lib/mce.cpp
@@ -7,24 +7,29 @@ using namespace ccr::mce;
 int generate (pubkey&pub, privkey&priv, prng&rng)
 {
 
+	return -1; //TODO
 }
 
 int pubkey::encrypt (const bvector& in, bvector&out, prng&rng)
 {
 
+	return -1; //TODO
 }
 
 int privkey::decrypt (const bvector&in, bvector&out)
 {
 
+	return -1; //TODO
 }
 
 int privkey::sign (const bvector&in, bvector&out, uint min_delta, prng&rng)
 {
 
+	return -1; //TODO
 }
 
 int pubkey::verify (const bvector&in, const bvector&hash, uint missing)
 {
 
+	return -1; //TODO
 }
--- lib/polynomial.cpp
+++ lib/polynomial.cpp
@@ -6,6 +6,7 @@ using namespace ccr;
 bool polynomial::is_irreducible()
 {
 
+	return false;
 }
 
 void polynomial::generate_random_irreducible (uint size, prng&rng)
