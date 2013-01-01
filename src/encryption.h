0e7b439f11c836323a59bb78bc254aafaf06ff0a
Mirek Kratochvil <exa.exa@gmail.com>
2013-01-01 17:22:19 +0100

    cosmetic fixes

--- src/encryption.h
+++ src/encryption.h
@@ -1,5 +1,4 @@
 
-
 /*
  * This file is part of Codecrypt.
  *
@@ -17,8 +16,8 @@
  * along with Codecrypt. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _ccr_hash_h_
-#define _ccr_hash_h_
+#ifndef _ccr_enc_h_
+#define _ccr_enc_h_
 
 
 #endif
