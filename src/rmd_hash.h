a09344f03469c89c915f2db15253f84f5f2be2c3
Mirek Kratochvil <exa.exa@gmail.com>
2016-04-17 15:47:47 +0200

    add proper copyright notices

--- src/rmd_hash.h
+++ src/rmd_hash.h
@@ -3,6 +3,8 @@
 /*
  * This file is part of Codecrypt.
  *
+ * Copyright (C) 2013-2016 Mirek Kratochvil <exa.exa@gmail.com>
+ *
  * Codecrypt is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at
